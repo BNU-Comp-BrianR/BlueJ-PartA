9cee1d1609af4bfdb156a0168e9c45609ef234cc
BrianRuszkowski <72171622+BrianRuszkowski@users.noreply.github.com>
2020-10-11 15:51:27 +0100

    Updated Course

--- app01-lab-classes/Course.java
+++ app01-lab-classes/Course.java
@@ -7,27 +7,24 @@
  */
 public class Course
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    // Attributes
+    private String title;
+    
+    private String codeNo;
+    
+    // Methods
 
     /**
      * Constructor for objects of class Course
      */
-    public Course()
+    public Course(String title, String codeNo)
     {
-        // initialise instance variables
-        x = 0;
+        this.title = title;
+        this.codeNo = codeNo;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    public void print()
     {
-        // put your code here
-        return x + y;
+        System.out.println("course: " + title + ", Code No: " + codeNo);
     }
 }
