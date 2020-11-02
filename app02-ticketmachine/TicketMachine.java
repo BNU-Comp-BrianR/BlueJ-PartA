0aa2df7a166b7d451adc8973c5b3799916b91343
BrianRuszkowski <72171622+BrianRuszkowski@users.noreply.github.com>
2020-11-02 15:33:43 +0000

    Added select ticket

--- app02-ticketmachine/TicketMachine.java
+++ app02-ticketmachine/TicketMachine.java
@@ -127,7 +127,7 @@ public class TicketMachine
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
             System.out.println("# Ticket");
-            System.out.println("# " + price + " cents.");
+            selectedTicket.print();
             System.out.println("##################");
             System.out.println();
 
