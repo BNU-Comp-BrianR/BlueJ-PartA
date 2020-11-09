5f6aa380d94af241695f166b8fba73f25befbac9
BrianRuszkowski <72171622+BrianRuszkowski@users.noreply.github.com>
2020-11-09 11:43:26 +0000

    Updated Package

--- app04-products/StockDemo.java
+++ app04-products/StockDemo.java
@@ -22,6 +22,27 @@ public class StockDemo
         manager.addProduct(new Product(101, "Samsung Galaxy S20"));
         manager.addProduct(new Product(102, "Apple iPhone 12"));
         manager.addProduct(new Product(103, "Google Pixel 4A"));
+        manager.addProduct(new Product(104, "Samsung Galaxy S20"));
+        manager.addProduct(new Product(105, "Apple iPhone 12"));
+        manager.addProduct(new Product(106, "Google Pixel 4A"));
+        manager.addProduct(new Product(107, "Samsung Galaxy S20"));
+        manager.addProduct(new Product(108, "Apple iPhone 12"));
+        manager.addProduct(new Product(109, "Google Pixel 4A"));
+        manager.addProduct(new Product(110, "Samsung Galaxy S20"));
+        manager.addProduct(new Product(111, "Apple iPhone 12"));
+        manager.addProduct(new Product(112, "Google Pixel 4A"));
+        
+    }
+    
+    public Product getProduct(int id)
+    {
+        Product product = manager.findProduct(id);
+        
+        if(product == null)
+        {
+            System.out.println("Product with ID: " + id + " is not recognised.");
+        }
+        return product;
     }
     
     /**
@@ -29,7 +50,7 @@ public class StockDemo
      * might be used. Details of one product are shown, the
      * product is restocked, and then the details are shown again.
      */
-    public void demoDeliverProduct()
+    public void runDemo()
     {
         // Show details of all of the products before delivery.
         manager.printProduct(101);
@@ -40,7 +61,5 @@ public class StockDemo
         // Show the list of all products after delivery
         manager.printProduct(101);
     }
-    
-
     
 }
--- app04-products/StockManager.java
+++ app04-products/StockManager.java
@@ -4,8 +4,8 @@ import java.util.ArrayList;
  * Manage the stock in a business.
  * The stock is described by zero or more Products.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (Brian Ruszkowski) 
+ * @version (08/11/2020)
  */
 public class StockManager
 {
@@ -37,44 +37,40 @@ public class StockManager
      */
     public void delivery(int id, int amount)
     {
+        Product product = findProduct(id);
+        if(product != null)
+        {
+           product.increaseQuantity(amount);
+           System.out.println("Product Delivered : " + product);
+        }
+        else
+        {
+            System.out.println("Product ID "+ id + " NOT FOUND!!!");
+        }
     }
     
-    public Product findProduct(int id)
-    {
-        return null;
-    }
-    
-    /**
+        /**
      * Sell one of the given item.
      * Show the before and after status of the product.
      * @param id The ID of the product being sold.
      */
-    public void sellProduct(int id)
+    public void sellProduct(int id, int quantity)
     {
         Product product = findProduct(id);
         
         if(product != null) 
         {
-            printProduct(id);
-            product.sellOne();
-            printProduct(id);
+           System.out.println( "Available stock :" + product.getQuantity());
+           product.sellOne();
+           System.out.println(product);
         }
-    }    
-
-    
-    /**
-     * Locate a product with the given ID, and return how
-     * many of this item are in stock. If the ID does not
-     * match any product, return zero.
-     * @param id The ID of the product.
-     * @return The quantity of the given product in stock.
-     */
-    public int numberInStock(int id)
-    {
-        return 0;
+        else
+        {
+            System.out.println( " Product not found");
+        }
     }
-
-    /**
+    
+        /**
      * Print details of the given product. If found,
      * its name and stock quantity will be shown.
      * @param id The ID of the product to look for.
@@ -89,17 +85,64 @@ public class StockManager
         }
     }
     
+        public Product findProduct(int id)
+    {
+          for(Product product : stock)
+        {
+            if(product.getID() == id)
+            {
+                return product;
+            }
+        }
+        return null;        
+    }  
+        
+    public void removeProduct(int id)
+    {
+        Product product = findProduct(id);
+        if(product != null)
+        {
+            stock.remove(product);
+            System.out.println("The product has been removed from Stock");
+        }
+    }
+         
+    public Product findByKeword(String productName)
+    {
+        for( Product product : stock)
+        {
+            if(product.getName().contains(productName))
+            {
+               return product;
+            }
+            else
+            {
+               System.out.println(" Product not Found!!! ");
+            }
+        }
+        return null;
+    }
+                               
+    public void changeProductName(int id, String replacement)
+    {
+        Product product = findProduct(id);
+        if(product != null)
+        {
+            changeProductName( id, replacement);
+        }
+        else
+        {
+            System.out.println("Product not found on the database");
+        }
+    }
+        
     /**
      * Print out each product in the stock
      * in the order they are in the stock list
      */
+    //change this.....................
     public void printAllProducts()
     {
-        System.out.println();
-        System.out.println("Peacock's Stock List");
-        System.out.println("====================");
-        System.out.println();
-        
         for(Product product : stock)
         {
             System.out.println(product);
