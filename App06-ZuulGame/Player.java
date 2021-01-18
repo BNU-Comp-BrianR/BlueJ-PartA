80e780c946d13b6bb3c6291f38b5d815531dece9
BrianRuszkowski <72171622+BrianRuszkowski@users.noreply.github.com>
2021-01-18 01:44:29 +0000

    Update Player

--- App06-ZuulGame/Player.java
+++ App06-ZuulGame/Player.java
@@ -7,27 +7,21 @@
  */
 public class Player
 {
-    // instance variables - replace the example below with your own
-    private int x;
+    private final String name;
+    private int score;
+    private int count;
+    
 
-    /**
-     * Constructor for objects of class Player
-     */
-    public Player()
+    public Player(String name)
     {
-        // initialise instance variables
-        x = 0;
+        this.name = name;
+        score = 0;
     }
-
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
+    
+    public void print()
     {
-        // put your code here
-        return x + y;
+        System.out.println("Player : " + name);
+        System.out.println();
     }
+    
 }
